29b6a3b89ef909b632f85ed7e755ff6e2f61e582
Hannes Tresch <h.tresch@gmail.com>
2012-12-13 21:14:30 +0100

    new fields for upload
    .name
    .description
    .visibility

--- upload.php
+++ upload.php
@@ -14,22 +14,51 @@
 <body>
 <table width="500" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
 <tr>
+
 <form action="upload_ac.php" method="post" enctype="multipart/form-data" name="form1" id="form1">
 <td>
+
 <table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+
 <tr>
 <td><strong>Upload Model</strong></td>
 </tr>
+
 <tr>
-<td>Select ZIP-file
-<input name="ufile" type="file" id="ufile" size="150" /></td>
+<td>Model name:</td> <td><input type="text" name="name"><br></td>
 </tr>
+
 <tr>
+<td>Visibility:</td> <td>
+<select name="visibility">
+  <option value="1">private</option>}
+  <option value="2">public</option>
+  <option value="3">friends</option>
+</select>
+
+</td>
+</tr>
+
+<tr>
+<td>Description:</td> <td><textarea name="description "></textarea><br></td>
+</tr>
+
+
+<tr>
+<td>Select ZIP-file</td>
+
+<td><input name="ufile" type="file" id="ufile" size="5" /></td>
+</tr>
+
+<tr>
+<td></td>
 <td align="center"><input type="submit" name="Submit" value="Upload" /></td>
 </tr>
+
 </table>
 </td>
 </form>
+
 </tr>
 </table> 
 </body>
