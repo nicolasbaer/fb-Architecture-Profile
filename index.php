8fa13690e0f5aae13d1777669337e29c653677fe
Hitiz <stefan.hilti@bluewin.ch>
2012-12-13 22:51:53 +0100

    etwas aufräumen

--- index.php
+++ index.php
@@ -60,8 +60,6 @@
 		
 		<!-- include details panel -->
 		<?php require_once('./library/php.inc/details.inc.php');?>
-		
-		<div id="altitude" style="display:none;"><p>Current altitude: N/A<br />Max altitude: N/A</p></div> 
 	
 	</body>
 </html>
